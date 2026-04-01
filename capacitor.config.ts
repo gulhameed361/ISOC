64aec9c877e9cb2c801b711ca19169b5ae029d97
Gul Hameed <g.hameed@surrey.ac.uk>
2026-04-01 14:02:48 +0100

    Fix: Added Firebase iOS config and URL schemes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,23 @@ const config: CapacitorConfig = {
   appId: 'com.surreyisoc.prayerroom',
   appName: 'ISOC Prayer Room',
   webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
   plugins: {
     FirebaseAuthentication: {
       skipNativeAuth: true,
       providers: ['google.com'],
     },
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999"
+    }
   },
 };
 
